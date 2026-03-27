ed9120bbc7bc708f4dc635bd54f2cf19357b903c
ItGlutos <gustavinpinhero123@gmail.com>
2026-03-27 15:10:03 -0300

    Implementação de outro tipo de relatório

--- relatorio_pagamento.py
+++ relatorio_pagamento.py
@@ -10,54 +10,122 @@ root = Tk()
 root.withdraw()
 root.attributes('-topmost', True)
 
+# comparação entre a primeira linha de cada relatório:
+# fbl1n
+comp1 = [None, None, 'St', 'Empresa', 'Fornecedo', None, 'LNeg', 'Referência', 'Nº documento', 'Data doc.', 'Doc.compensação', '   Mont.em MI', 'Vencimento', 'Texto', 'Tipo', 'GrpPrevTes', 'Compensaç.', 'BlP', 'Usuário']
+# rel_log
+comp2 = [None, 'Empresa', 'Loc.Neg', 'Fornecedor', 'Doc.Cont', None, None, 'Dat.Doc', 'Referência', 'Dt.Pagto', 'Tp.Doc', 'BIP', 'Montant.MI', 'Texto', 'Doc.Compen', 'Dat.Compen', 'Doc.MIRO', None, 'Dt.Process', 'Msg.LOG']
+
 possible_desktops = list(Path.home().glob("**/Desktop"))
 
 nome_arquivo = Path(simpledialog.askstring('Nome do Arquivo','Escolha o nome do seu arquivo')).with_suffix('.xlsx')
 
-# pasta = Path(filedialog.askdirectory(title='Qual pasta deseja usar?'))
-pasta = Path(r"C:\\Users\\gustavo.pinheiro\\OneDrive - DISTRIBUIDORA DE MEDICAMENTOS SANTA CRUZ LTDA\Automações pessoais\\Relatórios de pagamento")
+pasta = Path(filedialog.askdirectory(title='Qual pasta deseja usar?'))
 
 #para onde o arquivo será salvo
-# salvo_em = filedialog.askdirectory(title='Onde deseja salvar o seu relatório?')
-# salvo_em = Path(r"C:\\Users\\gustavo.pinheiro\\OneDrive - DISTRIBUIDORA DE MEDICAMENTOS SANTA CRUZ LTDA\\Desktop\\")
 salvo_em = possible_desktops
 
 arquivo_final = salvo_em[0] / nome_arquivo
-print(arquivo_final)
-
 arqvs = list(pasta.rglob('*.xls'))
 
 def tratamento(df):
+    first_line = list(df.iloc[0].where(pd.notnull(df.iloc[0]), None))
+    colunas_padrao = [
+        'Empresa', 'Fornecedor', 'LNeg', 'Referência', 'Nº documento', 
+        'Data doc.', 'Doc.compensação', 'Valor', 'Vencimento', 'Texto', 
+        'Tipo', 'Compensaç.', 'Blp'
+    ]
+
     
-    df = df.drop(df.columns[[0, 1, 2, 5]], axis=1)
+    if first_line == comp1:
 
-    # define o cabeçalho
-    df.columns = ['Empresa', 'Fornecedor', 'LNeg', 'Referêcia', 'Nº documento', 'Data doc.', 'Doc.compensação', 'Valor', 'Vencimento', 'Texto', 'Tipo', 'GrpPrevTes', 'Compensaç.','Blp', 'Usuário']
+        df = df.drop(df.columns[[0, 1, 2, 5, 15, 18]], axis=1)
+        df = df.drop(index=0).reset_index(drop=True)
 
-    df['Data doc.'] = df['Data doc.'].str.replace('.', '/')
-    df['Vencimento'] = df['Vencimento'].str.replace('.','/')
+        # define o cabeçalho
+        df.columns = colunas_padrao
 
-    df['Valor'] = df['Valor'].str.replace('.','')
-    df['Valor'] = df['Valor'].str.replace(',','.')
-    df['Valor'] = df['Valor'].astype(float)
+        df['Data doc.'] = df['Data doc.'].str.replace('.', '/')
+        df['Vencimento'] = df['Vencimento'].str.replace('.','/')
 
-    df = df[df['Tipo'].isin(['RE', 'KT','RF'])]
-    df['Valor'] = df['Valor']*(-1)
+        df['Valor'] = df['Valor'].str.replace('.','')
+        df['Valor'] = df['Valor'].str.replace(',','.')
+        df['Valor'] = df['Valor'].astype(float)
+
+        df = df[df['Tipo'].isin(['RE', 'KT','RF'])]
+        df['Valor'] = df['Valor']*(-1)
+
+    elif comp2 == first_line:
+
+        colunas_para_remover = df.columns[[0, 5, 6, 16, 17, 18, 19]]
+        df = df.drop(index=0).reset_index(drop=True)
+
+        colunas_renomeadas = {2:'LNeg',
+        9:'Vencimento',
+        10:'Tipo',
+        11:'Blp',
+        15:'Compensaç.',
+        12:'Valor',
+        14:'Doc.compensação',
+        7:'Data doc.',
+        3:'Fornecedor',
+        4:'Nº documento',
+        13:'Texto',
+        8:'Referência',
+        1:'Empresa'}
+
+        df = df.rename(columns=colunas_renomeadas)
+
+        df = df.drop(columns=colunas_para_remover)
+
+        df = df[['Empresa', 'Fornecedor', 'LNeg', 'Referência', 'Nº documento', 'Data doc.', 'Doc.compensação', 'Valor', 'Vencimento', 'Texto', 'Tipo', 'Compensaç.','Blp']]
+
+        df['Data doc.'] = df['Data doc.'].str.replace('.', '/')
+        df['Vencimento'] = df['Vencimento'].str.replace('.','/')
+
+        df['Valor'] = df['Valor'].str.replace('.','')
+        df['Valor'] = df['Valor'].str.replace(',','.')
+        df['Valor'] = df['Valor'].astype(float)
+
+        df = df[df['Tipo'].isin(['RE', 'KT','RF'])]
+
+    else:
+        messagebox.showerror("ERRO", "FORMATO DE ARQUIVO NÃO RECONHECIDO")
+        return None
 
     return df
 
+import pandas as pd
+
 try:
-    
 
     lista_dfs = []
 
-    import pandas as pd
+    qtd_colunas = list(range(20))
 
     for arq in arqvs:
-        df = pd.read_csv(arq, sep='\t',header=None ,encoding='utf-16', skiprows=9, dtype={
-                8:str,
-                10:str
-            })
+
+        df_probe1 = pd.read_csv(arq, sep='\t', header=None, encoding='utf-16', nrows=1, skiprows=8)
+        df_probe2 = pd.read_csv(arq, sep='\t', header=None, encoding='utf-16', nrows=1, skiprows=3)
+
+        first_line1 = list(df_probe1.iloc[0].where(pd.notnull(df_probe1.iloc[0]), None))
+        first_line2 = list(df_probe2.iloc[0].where(pd.notnull(df_probe2.iloc[0]), None))
+
+        if first_line1 == comp1:
+
+            df = pd.read_csv(arq, sep='\t',header=None ,encoding='utf-16', skiprows=8, dtype={
+                    8:str,
+                    10:str
+                })
+        elif first_line2 == comp2:
+
+            df = pd.read_csv(arq, sep='\t', skiprows=3, encoding='utf-16', index_col=False, header=None, names=qtd_colunas, engine='python')
+            # df = df.drop(index=0).reset_index(drop=True)
+            
+        else:
+            # messagebox.showerror('Erro Na consolidação',f'Formato de arquivo não reconhecido:\n{arq.name}')
+            continue
+
         df = tratamento(df)
         lista_dfs.append(df)
 
@@ -85,5 +153,3 @@ try:
 
 except Exception as e:
     messagebox.showerror('Erro', f'Seu arquivo não pôde ser gerado:\n{e}')
-
-    
